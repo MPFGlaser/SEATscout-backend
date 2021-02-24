ab2056ed6e0105b117afd3e3623a4b68fc4bc203
Max Glaser <mpf.glaser@gmail.com>
2021-02-24 12:17:34 +0100

    First setup of SpringBoot

---
+++ src/main/java/nl/mpfglaser/seatscoutbackend/SeatscoutBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.mpfglaser.seatscoutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatscoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatscoutBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mpfglaser/seatscoutbackend/SeatscoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mpfglaser.seatscoutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatscoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
