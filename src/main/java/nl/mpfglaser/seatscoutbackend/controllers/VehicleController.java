0a7b4b63cb815e62e7eff27ffcf25fcf5f491208
Max Glaser <mpf.glaser@gmail.com>
2021-03-26 17:55:41 +0100

    Fixed cross-origin access not being allowed

--- src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
@@ -6,25 +6,30 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 public class VehicleController {
     FakeDataStore fakeDataStore = new FakeDataStore();
 
+    @CrossOrigin()
     @GetMapping("/vehicles")
     public List<Vehicle> all() {
         return fakeDataStore.getAllVehicles();
     }
 
+    @CrossOrigin()
     @GetMapping("/vehicles/{name}")
     public Vehicle vehicle(@PathVariable String name) {
         return fakeDataStore.getVehicle(name);
     }
 
+    @CrossOrigin()
     @PostMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
     public Vehicle newVehicle(@RequestBody Vehicle newVehicle) {
         return fakeDataStore.addVehicle(newVehicle);
     }
 
+    @CrossOrigin()
     @PutMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
     public Vehicle updateVehicle(@RequestBody Vehicle updateVehicle) {
         return fakeDataStore.updateVehicle(updateVehicle);
