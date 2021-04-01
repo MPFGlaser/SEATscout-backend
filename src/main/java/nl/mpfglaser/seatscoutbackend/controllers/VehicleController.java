00a4016eaef5b0e80dda864bdf02107d1a73af4e
Max Glaser <mpf.glaser@gmail.com>
2021-04-01 10:14:09 +0200

    Removed all logic from VehicleController and placed it into VehicleService, thus making the controller "thin"

--- src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
@@ -1,7 +1,7 @@
 package nl.mpfglaser.seatscoutbackend.controllers;
 
 import nl.mpfglaser.seatscoutbackend.model.Vehicle;
-import nl.mpfglaser.seatscoutbackend.repository.FakeDataStore;
+import nl.mpfglaser.seatscoutbackend.service.VehicleService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,25 +9,25 @@ import java.util.List;
 @CrossOrigin()
 @RestController
 public class VehicleController {
-    FakeDataStore fakeDataStore = new FakeDataStore();
+    VehicleService vehicleService = new VehicleService();
 
     @GetMapping("/vehicles")
     public List<Vehicle> all() {
-        return fakeDataStore.getAllVehicles();
+        return vehicleService.all();
     }
 
     @GetMapping("/vehicles/{name}")
     public Vehicle vehicle(@PathVariable String name) {
-        return fakeDataStore.getVehicle(name);
+        return vehicleService.vehicle(name);
     }
 
     @PostMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
     public Vehicle newVehicle(@RequestBody Vehicle newVehicle) {
-        return fakeDataStore.addVehicle(newVehicle);
+        return vehicleService.newVehicle(newVehicle);
     }
 
     @PutMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
     public Vehicle updateVehicle(@RequestBody Vehicle updateVehicle) {
-        return fakeDataStore.updateVehicle(updateVehicle);
+        return vehicleService.updateVehicle(updateVehicle);
     }
 }
