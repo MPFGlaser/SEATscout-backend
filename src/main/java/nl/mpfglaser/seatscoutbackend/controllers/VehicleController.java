31e6c7f040fe7171e2dc086512a01c3438fdc3f0
Max Glaser <mpf.glaser@gmail.com>
2021-04-15 12:15:35 +0200

    Made the VehicleController return actual responses instead of just returning the object

--- src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/controllers/VehicleController.java
@@ -2,6 +2,7 @@ package nl.mpfglaser.seatscoutbackend.controllers;
 
 import nl.mpfglaser.seatscoutbackend.model.Vehicle;
 import nl.mpfglaser.seatscoutbackend.service.VehicleService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,27 +13,27 @@ public class VehicleController {
     VehicleService vehicleService = new VehicleService();
 
     @GetMapping("/vehicles")
-    public List<Vehicle> all() {
+    public ResponseEntity<List<Vehicle>> all() {
         return vehicleService.getAll();
     }
 
     @GetMapping("/vehicles/{id}")
-    public Vehicle vehicle(@PathVariable int id) {
+    public ResponseEntity<Vehicle> vehicle(@PathVariable int id) {
         return vehicleService.getById(id);
     }
 
     @GetMapping("/vehicles/type_name/{type_name}")
-    public Vehicle vehicle(@PathVariable String type_name) {
+    public ResponseEntity<Vehicle> vehicle(@PathVariable String type_name) {
         return vehicleService.getByTypeName(type_name);
     }
 
     @PostMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
-    public Vehicle newVehicle(@RequestBody Vehicle newVehicle) {
+    public ResponseEntity<Vehicle> newVehicle(@RequestBody Vehicle newVehicle) {
         return vehicleService.create(newVehicle);
     }
 
     @PutMapping(path = "/vehicles", consumes = "application/json", produces = "application/json")
-    public Vehicle updateVehicle(@RequestBody Vehicle updateVehicle) {
+    public ResponseEntity<Vehicle> updateVehicle(@RequestBody Vehicle updateVehicle) {
         return vehicleService.update(updateVehicle);
     }
 }
--- src/main/java/nl/mpfglaser/seatscoutbackend/service/VehicleService.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/service/VehicleService.java
@@ -2,29 +2,51 @@ package nl.mpfglaser.seatscoutbackend.service;
 
 import nl.mpfglaser.seatscoutbackend.model.Vehicle;
 import nl.mpfglaser.seatscoutbackend.repository.VehicleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class VehicleService {
     VehicleRepository vehicleRepository = new VehicleRepository();
 
-    public List<Vehicle> getAll() {
-        return vehicleRepository.getAll();
+    public ResponseEntity<List<Vehicle>> getAll() {
+        List<Vehicle> output = vehicleRepository.getAll();
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    public Vehicle getById(int id) {
-        return vehicleRepository.getById(id);
+    public ResponseEntity<Vehicle> getById(int id) {
+        Vehicle output = vehicleRepository.getById(id);
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    public Vehicle getByTypeName(String type_name){
-        return vehicleRepository.getByTypeName(type_name);
+    public ResponseEntity<Vehicle> getByTypeName(String type_name) {
+        Vehicle output = vehicleRepository.getByTypeName(type_name);
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    public Vehicle create(Vehicle newVehicle) {
-        return vehicleRepository.create(newVehicle);
+    public ResponseEntity<Vehicle> create(Vehicle newVehicle) {
+        Vehicle output = vehicleRepository.create(newVehicle);
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    public Vehicle update(Vehicle updateVehicle) {
-        return vehicleRepository.update(updateVehicle);
+    public ResponseEntity<Vehicle> update(Vehicle updateVehicle) {
+        Vehicle output = vehicleRepository.update(updateVehicle);
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
