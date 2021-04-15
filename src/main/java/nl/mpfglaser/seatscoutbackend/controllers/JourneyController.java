e579f0d9de3534ee992fb6bdc26eebca366289b6
Max Glaser <mpf.glaser@gmail.com>
2021-04-15 12:21:03 +0200

    Made the JourneyController return actual responses instead of just returning the object

--- src/main/java/nl/mpfglaser/seatscoutbackend/controllers/JourneyController.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/controllers/JourneyController.java
@@ -2,7 +2,11 @@ package nl.mpfglaser.seatscoutbackend.controllers;
 
 import nl.mpfglaser.seatscoutbackend.model.Journey;
 import nl.mpfglaser.seatscoutbackend.service.JourneyService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -12,12 +16,12 @@ public class JourneyController {
     JourneyService journeyService = new JourneyService();
 
     @GetMapping("/journeys")
-    public List<Journey> all(){
+    public ResponseEntity<List<Journey>> all() {
         return journeyService.all();
     }
 
     @GetMapping("/journeys/{id}")
-    public Journey journey(@PathVariable int id){
+    public ResponseEntity<Journey> journey(@PathVariable int id) {
         return journeyService.journey(id);
     }
 }
--- src/main/java/nl/mpfglaser/seatscoutbackend/service/JourneyService.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/service/JourneyService.java
@@ -2,17 +2,27 @@ package nl.mpfglaser.seatscoutbackend.service;
 
 import nl.mpfglaser.seatscoutbackend.model.Journey;
 import nl.mpfglaser.seatscoutbackend.repository.JourneyRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class JourneyService {
     JourneyRepository journeyRepository = new JourneyRepository();
 
-    public List<Journey> all(){
-        return journeyRepository.all();
+    public ResponseEntity<List<Journey>> all() {
+        List<Journey> output = journeyRepository.all();
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    public Journey journey(int id){
-        return journeyRepository.journey(id);
+    public ResponseEntity<Journey> journey(int id) {
+        Journey output = journeyRepository.journey(id);
+        if (output != null) {
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
