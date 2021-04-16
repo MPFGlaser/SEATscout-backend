4878f1aaa4210c85dc969919a4f0992657388c0b
Max Glaser <mpf.glaser@gmail.com>
2021-04-16 16:03:52 +0200

    Temporary fix for DTOs not converting to entities as they should

--- src/main/java/nl/mpfglaser/seatscoutbackend/repository/VehicleRepository.java
+++ src/main/java/nl/mpfglaser/seatscoutbackend/repository/VehicleRepository.java
@@ -19,13 +19,17 @@ public class VehicleRepository {
         return DB.find(Vehicle.class).where().eq("type_name", typeName).findOne();
     }
 
-    public Vehicle create(VehicleDTO newVehicle) {
+    public Vehicle create(VehicleDTO newVehicleDTO) {
+        // Should find a better & more graceful way to convert Entity <> DTO
+        Vehicle newVehicle = new Vehicle(newVehicleDTO.id, newVehicleDTO.typeName, newVehicleDTO.capacity);
         DB.save(newVehicle);
-        return DB.find(Vehicle.class).where().eq("id", newVehicle.id).findOne();
+        return DB.find(Vehicle.class).where().eq("id", newVehicle.getId()).findOne();
     }
 
-    public Vehicle update(VehicleDTO updateVehicle) {
+    public Vehicle update(VehicleDTO updateVehicleDTO) {
+        // Should find a better & more graceful way to convert Entity <> DTO
+        Vehicle updateVehicle = new Vehicle(updateVehicleDTO.id, updateVehicleDTO.typeName, updateVehicleDTO.capacity);
         DB.update(updateVehicle);
-        return DB.find(Vehicle.class).where().eq("id", updateVehicle.id).findOne();
+        return DB.find(Vehicle.class).where().eq("id", updateVehicle.getId()).findOne();
     }
 }
